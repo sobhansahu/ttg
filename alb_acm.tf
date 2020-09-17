6ba7530d0fab921a048b4cfa6544ef558a7fe99a
sobhansahu <62336624+sobhansahu@users.noreply.github.com>
2020-09-17 16:07:41 +0530

    Add files via upload

---
+++ alb_acm.tf
@@ -0,0 +1,104 @@
+provider “aws” {
+ region = “us-east-1”
+}
+
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default = "10.1.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default = "10.1.0.0/24"
+}
+variable "availability_zone" {
+  description = "availability zone to create subnet"
+  default = "us-east-2a"
+}
+
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance"
+  default = "ami-0cf31d971a3ca20d6"
+}
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default = "t2.micro"
+}
+variable "environment_tag" {
+  description = "Environment tag"
+  default = "Production"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "${var.cidr_vpc}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags {
+    "Environment" = "${var.environment_tag}"
+  }
+}
+
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags {
+    "Environment" = "${var.environment_tag}"
+  }
+}
+
+
+resource "aws_subnet" "subnet_public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  cidr_block = "${var.cidr_subnet}"
+  map_public_ip_on_launch = "true"
+  availability_zone = "${var.availability_zone}"
+  tags {
+    "Environment" = "${var.environment_tag}"
+  }
+}
+
+resource "aws_route_table" "rtb_public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+route {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+tags {
+    "Environment" = "${var.environment_tag}"
+  }
+}
+
+
+resource "aws_route_table_association" "rta_subnet_public" {
+  subnet_id      = "${aws_subnet.subnet_public.id}"
+  route_table_id = "${aws_route_table.rtb_public.id}"
+}
+
+resource "aws_security_group" "sg_22" {
+  name = "sg_22"
+  vpc_id = "${aws_vpc.vpc.id}"
+  ingress {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
+ egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags {
+    "Environment" = "${var.environment_tag}"
+  }
+}
+
+resource "aws_instance" "testInstance" {
+  ami           = "${var.instance_ami}"
+  instance_type = "${var.instance_type}"
+  subnet_id = "${aws_subnet.subnet_public.id}"
+  vpc_security_group_ids = ["${aws_security_group.sg_22.id}"]
+ tags {
+  "Environment" = "${var.environment_tag}"
+ }
+}
